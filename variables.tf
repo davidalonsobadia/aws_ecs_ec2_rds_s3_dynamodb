dd13e4e1a4bc480a67270bc5ef5fff80f09b7beb
David Alonso <david.alonsobadia@gmail.com>
2023-07-24 20:58:08 +0200

    add missing files

--- variables.tf
+++ variables.tf
@@ -1,19 +1,16 @@
 variable "rds_username" {
   type        = string
   description = "Username for Database"
-  default     = "admin"
 }
 
 variable "rds_password" {
   type        = string
   description = "Password for Database"
-  default     = "Administrator!123456"
 }
 
 variable "rds_db_name" {
   type        = string
   description = "Database name"
-  default     = "test_db"
 }
 
 variable "rds_port" {
